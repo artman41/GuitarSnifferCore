3da96bcc35848ae5681f0670a361bbe788da9484
Tyler Hughes <artman41@gmail.com>
2018-12-20 23:26:35 +0000

    Version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule GuitarSnifferCore.MixProject do
   def project do
     [
       app: :guitar_sniffer_core,
-      version: "1.1",
+      version: "1.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()
